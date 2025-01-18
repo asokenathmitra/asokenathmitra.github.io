4831578de3e8d0e86cb63432c8a8d8812a05beaa
Shivam-Singh-Aswal <shivamsinghaswal2002@gmail.com>
2025-01-18 17:08:39 +0530

    updated about

--- static/js/about.js
+++ static/js/about.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('load', function () {
     // Wait for 500ms before hiding the loading animation
     setTimeout(function () {
@@ -10,15 +11,8 @@ window.addEventListener('load', function () {
             elements[i].style.display = 'block';
         }
         // document.getElementsByClassName('the-page').style.display = 'none';
-        document.querySelector('nav').style.display = 'flex';
+        // document.getElementsByClassName('navbar').style.display = 'flex';
+        document.getElementsByClassName('mobile-topnav').style.display = 'flex';
+        // document.querySelector('navbar').style.display = 'flex';
     }, 500); // 500ms delay
 });
-
-// // script.js
-// window.addEventListener('load', function() {
-//     // Hide the loading animation
-//     document.getElementById('loading').style.display = 'none';
-
-//     // Show the main content
-//     document.getElementById('content').style.display = 'block';
-// });
