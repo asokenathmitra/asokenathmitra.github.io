f4ae1ec78e9a1da55011bd5076d5240134dff605
Shivam-Singh-Aswal <shivamsinghaswal2002@gmail.com>
2025-11-25 08:36:04 +0530

    Added the edition navigation dropdown Added pran nath page Copying this from the other repo, chakshika's

---
+++ README.md
@@ -0,0 +1,1 @@
+ Static website for A. N. Mitra memorial meeting and lecture 2025.
---
+++ static/js/about.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('load', function () {
+    // Wait for 500ms before hiding the loading animation
+    setTimeout(function () {
+        // Hide the loading animation
+        document.getElementById('loading').style.display = 'none';
+
+        // Show the main content
+        const elements = document.getElementsByClassName('the-page');
+        console.log(elements);
+        for (let i = 0; i < elements.length; i++) {
+            console.log("Removing ", elements[i]);
+            elements[i].style.display = 'block';
+        }
+        // document.getElementsByClassName('the-page').style.display = 'none';
+        // document.getElementsByClassName('navbar').style.display = 'flex';
+        document.getElementsByClassName('mobile-topnav').style.display = 'flex';
+        // document.querySelector('navbar').style.display = 'flex';
+    }, 500); // 500ms delay
+});
