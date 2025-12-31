48f2963886034454d9b4f92e7839322a9b4aa1bb
asokenathmitra <anmitra2025@gmail.com>
2025-12-31 13:33:31 +0530

    updated nav bar of mobile version and added skibinsky and kievski pages

--- static/js/navbarScript.js
+++ static/js/navbarScript.js
@@ -9,8 +9,19 @@ if (x.style.display === "block") {
 }
 }
 
-function mobile_dropdown_click() {
-    var x = document.getElementById("mobile-drop-container");
+function mobile_dropdown_edition_click() {
+    var x = document.getElementById("mobile-drop-edition-container");
+    var i;
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function mobile_dropdown_schedule_click() {
+    var x = document.getElementById("mobile-drop-schedule-container");
     var i;
 
     if (x.style.display === "block") {
